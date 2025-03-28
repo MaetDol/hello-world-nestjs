5ccac4c464e49370ceb5bc61fe8cc709784d889a
MaetDol <rndka9@gmail.com>
2025-03-29 01:01:23 +0900

    feat, shortUrl 엔티티 정의

--- src/short-url/entities/short-url.entity.ts
+++ src/short-url/entities/short-url.entity.ts
@@ -1,1 +1,10 @@
-export class ShortUrl {}
+import { Column, Entity, PrimaryColumn, Unique } from 'typeorm';
+
+@Entity()
+export class ShortUrl {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  url: string;
+}
