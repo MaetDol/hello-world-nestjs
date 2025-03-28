fe1eb3261ad08844561c7c35d52dc9b63a852e9e
MaetDol <rndka9@gmail.com>
2025-03-29 00:11:53 +0900

    feat, 아이디 타입 변경

--- src/short-url/dto/get-short-url.dto.ts
+++ src/short-url/dto/get-short-url.dto.ts
@@ -1,9 +1,9 @@
 export class GetShortUrlDto {
-  id: number;
+  id: string;
   url: string;
   originalUrl: string;
 
-  constructor(id: number, url: string, originalUrl: string) {
+  constructor(id: string, url: string, originalUrl: string) {
     this.id = id;
     this.url = url;
     this.originalUrl = originalUrl;
